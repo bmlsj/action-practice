5cd550e4f91b0e8163823e230e70c685d4ad2554
bmlsj <gaga2353@gmail.com>
2022-05-12 11:52:39 +0900

    d

--- diff.py
+++ diff.py
@@ -108,7 +108,7 @@ def main(b1, b2):
 	with open("json.txt", "w") as f:
 		f.write(json.dumps(tree))
 
-	result_tree = open('report_tree.md', 'w')
+	result_tree = open('report_tree.txt', 'w')
 	ptr(tree, result_tree)
 	result_tree.close()
 
